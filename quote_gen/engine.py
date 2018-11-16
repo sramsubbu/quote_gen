aba799614dd7fe6bf04ec5ed209f5741b6fc695a
Ramasubramanian S <sramsubu@gmail.com>
2018-11-16 17:06:20 +0530

    Added code to engine.py

--- quote_gen/engine.py
+++ quote_gen/engine.py
@@ -1,15 +1,35 @@
-def get_engine():
-    return Engine()
+import json
 
-class Engine:
+ENGINE = None
+DB_PATH = 'quotes.txt'
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(mcs, *args, **kwargs):
+        if mcs not in mcs._instances:
+            mcs._instances[mcs] = super().__call__(*args,*kwargs)
+        return mcs._instances[mcs]
+
+
+class Engine(metaclass=Singleton):
     def __init__(self):
-        pass
+        self.db_path = DB_PATH
+        with open(self.db_path) as fp:
+            self.data = json.load(fp)
 
-    def insert_row(self):
-        pass
+    def insert_row(self,record):
+        id = len(self.data)
+        self.data.append(record)
+        return id
 
-    def update_row(self):
-        pass
+    def update_row(self,record,record_id):
+        self.data[record_id] = record
 
+    def delete_row(self,record_id):
+        self.data.pop(record_id)
 
-
+    def commit(self):
+        with open(self.db_path,'w'):
+            json.dump(self.data)
