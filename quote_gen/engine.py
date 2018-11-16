4fcf04428eee8430a616562dad05b7573b61efac
Ramasubramanian S <sramsubu@gmail.com>
2018-11-16 16:44:01 +0530

    Added quote and engine modules. Added some structure for engine

---
+++ quote_gen/engine.py
@@ -0,0 +1,15 @@
+def get_engine():
+    return Engine()
+
+class Engine:
+    def __init__(self):
+        pass
+
+    def insert_row(self):
+        pass
+
+    def update_row(self):
+        pass
+
+
+
