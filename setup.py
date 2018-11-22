c7c281e7a0e799be2740b00cc54dc972756954eb
Ramasubramanian S <sramsubu@gmail.com>
2018-11-22 17:06:19 +0530

    Added a setup.py for the project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='quote_gen',
+    version='0.2',
+    packages=[ 'quote_gen',
+               'quote_gen.tests'],
+    url='',
+    license='GPL',
+    author='Ramasubramanian S',
+    author_email='sramsubu@gmail.com',
+    description='Generate a random quote from the list of available quotes'
+)
